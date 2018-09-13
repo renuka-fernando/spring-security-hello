4e9230ae4c6334b476bb5af4face93b82c112bda
Renuka Fernando <renuka.piyumal@gmail.com>
2018-09-13 12:20:13 +0530

    add security configurations

---
+++ src/main/java/com/renuka/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.renuka.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors().and()
+                .csrf().disable()
+                .authorizeRequests()
+                    .antMatchers("/greeting/open/**").permitAll()
+                    .anyRequest().authenticated();
+    }
+}
--- src/main/java/com/renuka/controllers/Greeting.java
+++ src/main/java/com/renuka/controllers/Greeting.java
@@ -8,12 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/greeting")
 public class Greeting {
-    @GetMapping("/{name}")
+    @GetMapping("/open/{name}")
     public String greet(@PathVariable("name") String name) {
         return "Hello " + name;
     }
 
-    @GetMapping("/{name}/protected")
+    @GetMapping("/protected/{name}")
     public String greetProtected(@PathVariable("name") String name) {
         return "Hello " + name + ", we have a secret for you. That is 4445-998 රේණුක!";
     }
