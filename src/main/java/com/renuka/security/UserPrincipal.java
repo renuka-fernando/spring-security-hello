525b09a7ef6c76d43066bebb0ada53927a382464
Renuka Fernando <renuka.piyumal@gmail.com>
2018-09-13 15:18:51 +0530

    add UserDetails

---
+++ src/main/java/com/renuka/security/UserPrincipal.java
@@ -0,0 +1,55 @@
+package com.renuka.security;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class UserPrincipal implements UserDetails {
+    private Long id;
+    private String name;
+    private String username;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    @JsonIgnore
+    private String email;
+
+    @JsonIgnore
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
